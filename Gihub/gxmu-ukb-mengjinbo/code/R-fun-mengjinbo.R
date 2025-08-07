a2ee1a956a62f69fc0e65ba2830a3f69a01a625d
apfqrmib <20225010198@sr.gxmu.edu.cn>
2025-08-08 00:04:57 +0800

    作业

--- Gihub/gxmu-ukb-mengjinbo/code/R-fun-mengjinbo.R
+++ Gihub/gxmu-ukb-mengjinbo/code/R-fun-mengjinbo.R
@@ -4,28 +4,44 @@ a <- data.frame(
   a3 = c(4,1,9)
 )
 head(a)
-
-?pmin
 pmin(a$a1,a$a2,a$a3)
 min(a$a1,a$a2,a$a3)
-
 pmin_apply<-function(...){
   apply(..., 1, min)
   }
 pmin_apply(a)
-
 pmin_for <- function(...) {
   result <- numeric(nrow(...))
   for (i in 1:nrow(...)) {
     result[i] <- min(as.numeric(...[i, ]))
   }
-  
   return(result)
 }
 pmin_for(a)
-
 install.packages("bench")
 bench::mark(base=pmin(a$a1,a$a2,a$a3),
             pmin_apply=pmin_apply(a),
             pmin_for=pmin_for(a)
-)
+            )
+b1<-data.frame(
+  name= c("文", "颜", "唐", "黄"),
+  score = c(95, 96, 97, 98)
+  )
+print(b1)
+b2<-data.frame(
+  name=c("文", "颜"),
+  github=c("uuu1016","yanyutong111")
+  )
+print(b2)
+?merge
+b<-merge(b1,b2,by="name")
+print(b)
+install.packages("dplyr")
+c<-inner_join(b1, b2, by = "name")
+print(c)
+"%merge%"<-function(x, y){
+  by <- intersect(names(x), names(y))
+  finish<-merge(x, y, by = by)
+  print (finish)
+}
+b1%merge%b2
